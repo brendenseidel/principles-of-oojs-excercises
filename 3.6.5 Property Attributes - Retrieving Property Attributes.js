c231d404be128016beaa2fb58ac25e4133dca1a5
Brenden Seidel <brenseid@gmail.com>
2017-11-10 20:42:01 -0500

    3.6.5 excercise content

--- 3.6.5 Property Attributes - Retrieving Property Attributes.js
+++ 3.6.5 Property Attributes - Retrieving Property Attributes.js
@@ -1,3 +1,19 @@
 // ------------------------------------
 // 3.6.5 Retrieving Property Attributes
 // ------------------------------------
+
+var france = {
+    capital: "Paris",
+    romantic: true,
+    knownFor: ["le fromage", "wine", "des chats", "tour de france", "cafes"]
+};
+
+capitalPropertyAttribute = Object.getOwnPropertyDescriptor(france, "capital");
+
+console.log(capitalPropertyAttribute);
+// { value: 'Paris', writable: true, enumerable: true, configurable: true }
+
+console.log(capitalPropertyAttribute.value); //'Paris'
+console.log(capitalPropertyAttribute.writable); //true
+console.log(capitalPropertyAttribute.enumerable); //true
+console.log(capitalPropertyAttribute.configurable); //true
