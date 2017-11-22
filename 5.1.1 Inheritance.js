238edde5f3fb4d156ea3bc4125b9031d68e5e9ca
Brenden Seidel <brenseid@gmail.com>
2017-11-22 09:54:20 -0500

    5.1.1 Inheritance, pgs. 66-68 - Methods Inherited from Object.prototype

---
+++ 5.1.1 Inheritance.js
@@ -0,0 +1,44 @@
+// -----------------------------------------
+// 5.1.1 Inheritance, pgs. 66-68
+// - Methods Inherited from Object.prototype
+// -----------------------------------------
+
+// ## valueOf() pg. 67 ##
+var mountain = {
+  elevation: 20310,
+  name: "Denali"
+};
+
+console.log(mountain.valueOf()); // { elevation: 20310, name: 'Denali' }
+
+// valueOf is called whenever an
+// operator is used on an object...
+var now = new Date();
+var earlier = new Date(2010, 1, 1);
+
+// ...thus, these are doing the same thing
+console.log(now.valueOf() > earlier.valueOf()); // true
+console.log(now > earlier); // true
+
+
+// ## toString() pg. 67 ##
+// - called whenever valueOf() returns
+//   a reference instead of a primitive
+// - implicitly called on primitives
+//   when JS is expecting a string
+
+console.log("Mountain = " + mountain); // Mountain = [object Object]
+
+// Modifying to string to return the mountain's
+mountain.toString = function() {
+  return "[Mountain: " + this.name + "]";
+};
+
+console.log("Mountain = " + mountain); // Mountain = [Mountain Denali]
+
+// ----------------------------------
+
+// these are doing the same thing
+console.log("The elevation of " + mountain.name + " is " + mountain.elevation + " feet.");
+console.log("The elevation of " + mountain.name + " is " + mountain.elevation.toString() + " feet.");
+
